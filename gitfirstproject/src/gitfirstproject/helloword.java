dfb0dc8d6d8e9f39278e64c88e9261f8f9c7c418
rashmijg <rashmiskp222@gmail.com>
2021-11-16 18:38:25 +0530

    modified hello word

--- gitfirstproject/src/gitfirstproject/helloword.java
+++ gitfirstproject/src/gitfirstproject/helloword.java
@@ -4,7 +4,7 @@ public class helloword {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-     System.out.print("hello");
+     System.out.print("hello word");
 	}
 
 }
