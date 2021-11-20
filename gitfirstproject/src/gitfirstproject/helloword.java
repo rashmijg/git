1bb1d1c038852d0e61d5aea7224392d0ffe7590e
rashmijg <rashmiskp222@gmail.com>
2021-11-20 19:40:29 +0530

    new commit for new project for jenkins

--- gitfirstproject/src/gitfirstproject/helloword.java
+++ gitfirstproject/src/gitfirstproject/helloword.java
@@ -4,7 +4,7 @@ public class helloword {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-     System.out.print("hello word main branch commit");
+     System.out.print("hello word main branch committed");
 	}
 
 }
